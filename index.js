0addcb74c20ac135a107597d35eeb9c54cfd4e8b
mooxiu <monetshaw@outlook.com>
2023-01-25 00:44:28 +0900

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+selfDefinedCommands = [
+    {
+        prompt: '❓ Why, but why',
+        displayed: `<span style="background-color: orange; color: papayawhip; font-size: larger; font-style: italic; font-weight: bold">WHY?</span>`,
+    },
+    {
+        prompt: '❓ What the fuck is this?',
+        displayed: `<span style="background-color: red; color: papayawhip; font-size: larger; font-style: italic; font-weight: bold">WHAT?</span>`,
+    }
+]
+
+function main() {
+    selfDefinedCommands.forEach(
+        c => {
+            logseq.Editor.registerSlashCommand(
+                c.prompt,
+                async () => {
+                    logseq.Editor.insertAtEditingCursor(c.displayed)
+                },
+            )
+        });
+}
+
+// bootstrap
+logseq.ready(main).catch(console.error);
